ac486a64a4cd72b9477cf28c697e2901fa98ba1e
dkyoung2004 <dkyoung0328@gmail.com>
2020-11-19 18:17:00 +0900

    go
    3일차 연습

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+func lenAndUpper(name string) (int, string) {
+
+	return len(name), strings.ToUpper(name)
+
+}
 func main() {
 	// 2일차
 	// something.SayHello()
 	// something.saybye() 는 소문자로 시작하는 private 함수라서 반응하지 않음
 	// ctrl+click하면 원하는 정보를 다 볼 수있다.
-	name := "nico"
 	// same with var name string = "nico"
-	name = "lynn"
-	fmt.Println(name)
-	fmt.Println(multiply(2, 2))
+	totalLength, upperName := lenAndUpper("nico")
+	fmt.Println(totalLength, upperName)
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
 }
 
 func multiply(a, b int) int {
 	return a * b
 
+}
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
